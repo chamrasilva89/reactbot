098c323044a87437151c9d4233490833cba14987
Chamara Silva <chamara.silva@pronto.net>
2023-03-13 10:59:58 +0530

    settings update

--- config/prod.js
+++ config/prod.js
@@ -1,7 +1,16 @@
 module.exports = {
-  googleProjectID: process.env.GOOGLE_PROJECT_ID,
-  dialogFlowSessionID: process.env.DIALOGFLOW_SESSION_ID,
-  dialogFlowSessionLanguageCode: process.env.DIALOGFLOW_LANGUAGE_CODE,
-  googleClientEmail: process.env.GOOGLE_CLIENT_EMAIL,
-  googlePrivateKey: process.env.GOOGLE_PRIVATE_KEY,
+  googleProjectID: "cag-lfqv",
+  dialogFlowSessionID: "react-bot-session",
+  dialogFlowSessionLanguageCode: "en-US",
+  googleClientEmail: "dialogflow-client@cag-lfqv.iam.gserviceaccount.com",
+  googlePrivateKey:
+    "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQC5JfXaXBe9dXFU\nj7j739aNlr64ivGoEOh7lPbUsIxKraz0VP5L0R/8alhhocMogg1k2De4mU+AM07c\ndYQwvWDnvzwAEV4AVThcqT1dFbUucuXpeBPge5dw+C/emPmrG3yIX0YzE24pBptc\n7dz61OKK4GVE6auO5OomIrJzoTNfnfu6pxqNaTxB3iSBKHWO3KiNRjK/bMU9qDR/\nBhGZW/qIFkv7pPox+TEivlV5SnzJgSCrwBQ96XqdSJh8AUtnVMftSLX7qt9bd7k2\n0nVC/DcIDACn+4V4rGZbjuOJ4hS/SbJi/67ZwWfr2nZZmXHXlnQKAydw1WR/rODA\nz6VujIKFAgMBAAECggEACABppqfnksA6R02gJf/fgDutPxZ/fnmfQoVb7DHmbkJ2\nlhH88ik1460ePWHwqECD9FbFLwzZJbuR7e8vk60A53hupGYbv5HvSfnlIAUUe88V\nZp385e0Rd3K9HDaqRkh9fPtOcl368HpLnpwyZQNsTaozAOPEY/w6rlk+MxxwDEgZ\nk0cOUq12+5FxI2zNvHp2aO5GRUjcE9/V01Pn8QkPpcgOjJG+AmpmtRagqSK6g6lV\nbgUmX2sIrAbHKqXN2OdJJbTkt5G2rgiYQindUyLNNGs500f4HFeT+aAzb50NHE+n\n7t5IteNtcPrYgFuzKIpu4R1fNRsZ7fdexRKN1BkxIQKBgQD9vxgQazhmQcR9KJ/2\nIsQZXM2xe+rnFyns9x5dNPdmPWm2cm68MbspdsvZiNEDm3VgxyXqvbxk8P43SF+f\n0tXFuBb5RFmnaUU8nMwMBNrtC4KpfZgIDRxaN2RXA8Few+P6Yg9eSGzaWkEG7q8q\nQOQg64wx+Yw+CpNGS4mzskMoqQKBgQC6yuefacO99H4ZCCf7dwtu+g600qpEZlAW\n+r+MqIM/lc+Ifn1xedVDSAqrcWNrxIUwPU7GmTTCXORZDwc/iU53pgVhU0hWnAFp\nV2Z551GsovyHalbkGW5V2MTzZg6wYCMksHJLyrZR0VMppvkGo/W2u9y7sm4GJ19A\n7+VsFnVofQKBgQDuFcNKhJAiwkrO9LNCi1RcR8toYNEDi6R3B1eiNPR9Jdpuqehl\nBHSMbz7OGaWkMNVtvO5C10xRmh2tWWb/2x9hqV1E/XDxt8CTIrwB+vw/trEevw1L\nYKHdtGOdkZR6imr6n8+zs1kPd+fTzVjl2IU0rk6ZBYElJV8ScgX+aaDCIQKBgG28\n4tLOqgag6qv+FK8xQvBRy+4DjbBdg1PcwEf8gA8NDj2cQko2T0oVut754HThoXWz\nruISA+Lj++suXw2+tvCoAqUo7mijgljG1hdsBJmtrzpHCQE8x4t7rElqfdq0wDC/\n0IYic3Yhyf/AbrCuBp7yQz2qF+VZoCZgSaJ0l0lpAoGAWL3jzKfdptpFpgKmLnaQ\ni3p3pHILPgx9AI6RinWxFp8+sT584mAlNY7KTLqR+s1ZlVzeAiZOepCiiAQfGqoa\nDOGu/DOOOjMmBGJCHdgUeqGA3gKIyksADpUCCwLgpstDMhK7b4kllV9h3Ab9pYjA\n/ZqUr80L+8KIjktdtx8hwJE=\n-----END PRIVATE KEY-----\n",
 };
+
+//module.exports = {
+//  googleProjectID: process.env.GOOGLE_PROJECT_ID,
+//  dialogFlowSessionID: process.env.DIALOGFLOW_SESSION_ID,
+//  dialogFlowSessionLanguageCode: process.env.DIALOGFLOW_LANGUAGE_CODE,
+//  googleClientEmail: process.env.GOOGLE_CLIENT_EMAIL,
+//  googlePrivateKey: process.env.GOOGLE_PRIVATE_KEY,
+//};
